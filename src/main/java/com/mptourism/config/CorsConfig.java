3bc241579d3258f8ca74a277c1b7a821d7b5ec72
Sagar Tiwari <sagartwr18@gmail.com>
2026-02-11 11:31:03 +0530

    Frontend Origin Added

--- src/main/java/com/mptourism/config/CorsConfig.java
+++ src/main/java/com/mptourism/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("https://mp-tourism-frontend.vercel.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(false);

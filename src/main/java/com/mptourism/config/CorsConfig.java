081e37e0344bd0bda56d1171b7032c3d2208e37b
Sagar Tiwari <sagartwr18@gmail.com>
2026-02-12 10:21:05 +0530

    Localhost origin added

--- src/main/java/com/mptourism/config/CorsConfig.java
+++ src/main/java/com/mptourism/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://mp-tourism-frontend.vercel.app/", "https://mp-tourism-admin-panel.vercel.app/"));
+        configuration.setAllowedOrigins(Arrays.asList("https://mp-tourism-frontend.vercel.app/", "https://mp-tourism-admin-panel.vercel.app/", "http://localhost:5173/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(false);
